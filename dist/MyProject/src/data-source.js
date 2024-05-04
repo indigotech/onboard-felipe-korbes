5491a0114added8edcf95d8c4b9d075dea1da318
luis-fk <felipe.korbes@taqtile.com.br>
2024-05-04 11:58:23 -0300

    docker files setup

---
+++ dist/MyProject/src/data-source.js
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "admin",
+    password: "123",
+    database: "Users",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: []
+});
