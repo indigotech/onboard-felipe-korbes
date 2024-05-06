7d459477d96c3d742a50d026e769412c32604d25
luis-fk <felipe.korbes@taqtile.com.br>
2024-05-06 16:56:51 -0300

    comments removed

--- src/index.ts
+++ src/index.ts
@@ -5,39 +5,25 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 const prisma = new PrismaClient();
 
 async function main() {
-  // await prisma.user.create({
-  //   data: {
-  //     name: "John Doe",
-  //     email: "john@prisma.io",
-  //     age: 33,
-  //   },
-  // });
-
-  // const post = await prisma.user.update({
-  //   where: { id: 1 },
-  //   data: { email: "felipe.korbes@taqtile.com.br" },
-  // });
-  // console.log(post);
-
   const allUsers = await prisma.user.findMany({
     where: {
       name: {
-        contains: "Felipe",
-      },
-    },
+        contains: "Felipe"
+      }
+    }
   });
   console.log(allUsers);
 }
 
 main()
-.then(async () => {
-  await prisma.$disconnect();
-})
-.catch(async (e) => {
-  console.error(e);
-  await prisma.$disconnect();
-  process.exit(1);
-});
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 const typeDefs = `#graphql
     type Query {
@@ -47,19 +33,19 @@ const typeDefs = `#graphql
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
-  },
+    hello: () => "Hello world!"
+  }
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 (async () => {
   try {
     const { url } = await startStandaloneServer(server, {
-      listen: { port: 4001 },
+      listen: { port: 4001 }
     });
 
     console.log(`🚀 Server ready at: ${url}`);
