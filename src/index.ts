02c668e0bb22659bee8d8f8a7c76c5e5d4bcf5a3
luis-fk <felipe.korbes@taqtile.com.br>
2024-05-05 21:06:06 -0300

    connection to the db and apollo implemented

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import "reflect-metadata";
+
+// make a connection to the database and adds a user to it
+AppDataSource.initialize()
+  .then(async () => {
+    // console.log("Inserting a new user into the database...");
+    // const user = new User();
+    // user.firstName = "Felipe";
+    // user.lastName = "Korbes";
+    // user.age = 28;
+    // await AppDataSource.manager.save(user);
+    // console.log("Saved a new user with id: " + user.id);
+
+    // console.log("Loading users from the database...");
+    // const users = await AppDataSource.manager.find(User);
+    // console.log("Loaded users: ", users);
+
+    // console.log(
+    //   "Here you can setup and run express / fastify / any other framework."
+    // );
+    const users = await AppDataSource.manager.find(User);
+    console.log(users);
+
+    const userFirstName = await AppDataSource.manager.findOneBy(User, {
+      firstName: "Felipe",
+    });
+    console.log(userFirstName.lastName);
+  })
+  .catch((error) => console.log(error));
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -12,26 +42,27 @@ const typeDefs = `#graphql
 `;
 
 // Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    hello: () => "Hello world!"
-  }
+    hello: () => "Hello world!",
+  },
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-});
+(async () => {
+  try {
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: 4001 },
+    });
 
-console.log(`🚀  Server ready at: ${url}`);
+    console.log(`🚀 Server ready at: ${url}`);
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+})();
