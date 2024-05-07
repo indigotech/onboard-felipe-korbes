7a4238ef74fb07267d46f1fcf403ac1ae86721d5
luis-fk <felipe.korbes@taqtile.com.br>
2024-05-07 16:39:29 -0300

    test for the hello query

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,24 @@
+import axios from "axios";
+import server from "../src/index"; // Import the server setup
+import { describe, before, it, after } from "mocha"; // Mocha imports
 import assert from "assert";
 
-describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe("GraphQL Server Test", function () {
+  let serverUrl: string;
+
+  before(async function () {
+    serverUrl = "http://localhost:4000";
+  });
+
+  it("should return 'Hello world!' from the hello query", async function () {
+    const response = await axios.post(serverUrl, {
+      query: "{ hello }"
     });
+
+    assert.equal(response.data.data.hello, "Hello world!");
+  });
+
+  after(async function () {
+    await server.stop();
   });
 });
