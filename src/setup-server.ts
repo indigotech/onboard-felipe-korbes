8769848e8645f730df80a58d499acad355032e52
luis-fk <felipe.korbes@taqtile.com.br>
2024-05-09 17:12:16 -0300

    small change

--- src/setup-server.ts
+++ src/setup-server.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./graphql/schema";
 import { resolvers } from "./graphql/resolvers";
-import { formatError } from "./graphql/error-handler";
+import { formatError } from "./graphql/error-handlers";
 
 export let server: ApolloServer;
 export let url: string;
@@ -16,11 +16,10 @@ export async function setupServer() {
     formatError
   });
 
-  const { url: getUrl } = await startStandaloneServer(server, {
+  const { url } = await startStandaloneServer(server, {
     listen: { port: parseInt(process.env.PORT ?? "4002") }
   });
 
-  url = getUrl;
   console.log("Server setup complete");
   console.log(`Server ready at: ${url}`);
 }
