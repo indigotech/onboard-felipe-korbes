bde889295ca72a298d523ee66b0e3d8f503f7061
luis-fk <felipe.korbes@taqtile.com.br>
2024-05-13 09:30:22 -0300

    added more tests

--- test/login-user-test.ts
+++ test/login-user-test.ts
@@ -46,4 +46,64 @@ describe("Login authentication tests", function () {
 
     assert.equal(response.data.data.login.token, "tokenTest");
   });
+
+  it("Failed to login with the wrong password", async function () {
+    const hashedPassword = await hashPassword("123abc");
+
+    await prisma.user.create({
+      data: {
+        name: "User Test",
+        email: "test@example.com",
+        password: hashedPassword,
+        birthDate: "01-01-2000"
+      }
+    });
+
+    const response = await axios.post(url, {
+      query: loginUserMutation({
+        email: "test@example.com",
+        password: "123abc1"
+      })
+    });
+
+    expect(response.data.errors).to.be.deep.eq([
+      {
+        code: 400,
+        message: "Wrong password and/or email"
+      }
+    ]);
+  });
+
+  it("Failed to login with a non existing email", async function () {
+    const hashedPassword = await hashPassword("123abc");
+
+    await prisma.user.create({
+      data: {
+        name: "User Test",
+        email: "test@example.com",
+        password: hashedPassword,
+        birthDate: "01-01-2000"
+      }
+    });
+
+    const createdUserDB = await prisma.user.findUnique({
+      where: {
+        email: "test@example.com"
+      }
+    });
+
+    const response = await axios.post(url, {
+      query: loginUserMutation({
+        email: "test1@example.com",
+        password: "123abc"
+      })
+    });
+
+    expect(response.data.errors).to.be.deep.eq([
+      {
+        code: 400,
+        message: `User with email test1@example.com not found`
+      }
+    ]);
+  });
 });
